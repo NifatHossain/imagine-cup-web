8a0c216f101648b09458948f99341f483542f41b
NifatHossain <nifathossain1480@gmail.com>
2024-02-03 12:06:19 +0600

    nav responsive for small mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       screens: {
+        'xsm': '360px',
+        
         'sm': '640px',
         // => @media (min-width: 640px) { ... }
   
         'md': '768px',
         // => @media (min-width: 768px) { ... }
-        
+
   
         'lg': '1024px',
         // => @media (min-width: 1024px) { ... }
